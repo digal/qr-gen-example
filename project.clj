1b0f0b15f048a4f9fff7123bd9a33c537b902c22
Yuri Buyanov <yuri.buyanov@gmail.com>
2014-02-10 16:54:19 +0400

    min-lein-version

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring "1.2.1"]
                  [com.google.zxing/javase "2.1"]
